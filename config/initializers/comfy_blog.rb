8c5ab039aaaac2f3087fe4a1f0019b165479144c
Stephen McLeod <stephen.m.mcleod@gmail.com>
2013-04-19 05:19:51 -0400

    update adminconfig stuff

--- config/initializers/comfy_blog.rb
+++ config/initializers/comfy_blog.rb
@@ -1,8 +1,5 @@
 ComfyBlog.configure do |config|
   
-  # set the cms admin path if you have changed it for CMS
-  #   config.admin_route_prefix = 'admin'
-  
   # Controller that should be used for admin area
   #   config.admin_controller = 'ApplicationController'
   
--- lib/comfy_blog/engine.rb
+++ lib/comfy_blog/engine.rb
@@ -7,11 +7,9 @@ module ComfyBlog
       if defined?(ComfortableMexicanSofa)
         # Applying configuraion
         ComfyBlog.configure do |conf|
-          # conf.admin_route_prefix = ComfortableMexicanSofa.config.admin_route_prefix
-          conf.admin_route_prefix = ComfortableMexicanSofa::Routing.admin
-          conf.admin_route_prefix = 'admin'
-          conf.admin_controller = 'CmsAdmin::BaseController'
-          conf.form_builder = 'ComfortableMexicanSofa::FormBuilder'
+          conf.admin_route_prefix   = ComfortableMexicanSofa::Routing.admin
+          conf.admin_controller     = 'CmsAdmin::BaseController'
+          conf.form_builder         = 'ComfortableMexicanSofa::FormBuilder'
         end
         # Adding view hooks
         ComfortableMexicanSofa::ViewHooks.add(:navigation, '/admin/blog/navigation')
